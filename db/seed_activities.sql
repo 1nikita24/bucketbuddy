7968c12f56e4877f0e5711eedc5fe41365ed12ad
Jenks <mikedjenk@gmail.com>
2019-08-09 14:39:17 -0400

    seeds for all tables working

---
+++ db/seed_activities.sql
@@ -0,0 +1,14 @@
+USE bucketbuddy_db;
+
+INSERT INTO activities (activity, categoryId)
+VALUES
+    ("Go Skydiving", 1),
+    ("Run a Marathon",5),
+    ("Learn a New Language",3),
+    ("Go to Alaska",2),
+    ("Go Camping with a Significant Other",4),
+    ("Live in a Hobbit Hole",6),
+    ("Go to the Super Bowl",5),
+    ("Ride a Tandom Bike", 5),
+    ("Meet Bill Gates", 6),
+    ("Climb Mt.Everst", 1)
