9cb16b4252560692f87cd84fcd83f0fa779e47ba
1niita24 <nikitaezhov@icloud.com>
2019-08-11 12:04:06 -0400

    adding routes

--- db/connection.js
+++ db/connection.js
@@ -3,27 +3,27 @@ var mysql = require("mysql");
 var connection;
 
 if (process.env.JAWSDB_URL) {
-   connection = mysql.createConnection(process.env.JAWSDB_URL);
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-  connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "password",
-    database: "bucketbuddy_db"
-  });
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "May04month",
+        database: "bucketbuddy_db"
+    });
 }
 
 connection.connect(function(err) {
-  if (err) throw err;
-  console.log("connected as id " + connection.threadId);
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
 });
 
 connection.config.typeCast = function(field, next) {
-  if (field.type == "TINY" && field.length == 1) {
-    return field.string() == "1"; // 1 = true, 0 = false
-  }
-  return next();
+    if (field.type == "TINY" && field.length == 1) {
+        return field.string() == "1"; // 1 = true, 0 = false
+    }
+    return next();
 };
 
 module.exports = connection;
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,18 +3,29 @@ var connection = require("../db/connection");
 module.exports = function(app) {
 
     // ============ Express App TEST ============
-  app.get("/api/testserver", function(req, resExpress) {
-    resExpress.send("Express server is working - TEST PASSED!");
-  });
-
-  // ============ Database TEST ============
-  app.get("/api/testdb", function(req, resExpress) {
-    connection.query("SELECT * FROM notes", function(err, resSQL) {
-      if (err) throw err;
-    //   console.log(resSQL);
-    //   console.log("mySQL DB working! TEST PASSED");
-      resExpress.send("mySQL DB working! TEST PASSED");
+    app.get("/api/testserver", function(req, resExpress) {
+        resExpress.send("Express server is working - TEST PASSED!");
     });
-   
-  });
+
+    // ============ Database TEST ============
+    app.get("/api/testdb", function(req, resExpress) {
+        connection.query("SELECT * FROM notes", function(err, resSQL) {
+            if (err) throw err;
+            //   console.log(resSQL);
+            //   console.log("mySQL DB working! TEST PASSED");
+            resExpress.send("mySQL DB working! TEST PASSED");
+        });
+
+    });
+
+    // search(GET)/api/search{select/where}
+
+    // results(PUT)api/add{insert/into}
+
+    // delete(DELETE)api/delete{delete/from}
+
+    // list(GET)api/mylist{select/where/userid}
+
+    // buddies(GET)api/buddies{select/where/[usD1]userID+activID=[usD2]userID+activID}
+
 };
