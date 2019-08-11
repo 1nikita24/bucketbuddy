0f53e79f919ba71b0b77db4208e08b4c4e243501
Jenks <mikedjenk@gmail.com>
2019-08-11 11:31:44 -0400

    local

--- config/orm.js
+++ config/orm.js
@@ -3,12 +3,46 @@ var connection = require("../db/connection.js");
 var orm = {
   selectAll: function(whatToSelect, tableInput, cb) {
     var queryString = "SELECT ?? FROM ??";
-    console.log(whatToSelect + ":" + tableInput );
-    connection.query(queryString, [whatToSelect, tableInput], function(err, result) {
+    console.log(whatToSelect + ":" + tableInput);
+    connection.query(queryString, [whatToSelect, tableInput], function(
+      err,
+      result
+    ) {
       if (err) throw err;
+      // console.log(result);
       cb(result);
     });
-  }  
-};
+  },
+  selectUserPofile: function(tableInput, colToSearch, valOfCol, cb) {
+    var queryString = "SELECT * FROM ?? WHERE ?? = ?";
+
+    connection.query(queryString, [tableInput, colToSearch, valOfCol], function(
+      err,
+      result
+    ) {
+      if (err) throw err;
+      // console.log(result);
+      cb(result);
+    });
+  },
+  selectMyList: function(valOfCol, cb) {
+    var queryString =
+      "SELECT userProfiles.uid, userProfiles.name, bucketLists.activityId, activities.activity, activities.categoryId, categories.category";
+    queryString += " FROM userProfiles";
+    queryString +=
+      " INNER JOIN bucketLists ON userProfiles.id = bucketLists.userId";
+    queryString +=
+      " INNER JOIN activities ON bucketLists.activityId = activities.id";
+    queryString +=
+      " INNER JOIN categories ON activities.categoryId = categories.id";
+    queryString += " WHERE userProfiles.uid = ?";
+
+    connection.query(queryString, [valOfCol], function(err, result) {
+      if (err) throw err;
+      // console.log(result);
+      cb(result);
+    });
+  }
+}; // end orm var
 
 module.exports = orm;
