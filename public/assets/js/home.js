8516d805ee594d7700e77152a5033a59ed695e4a
Vanessa de La Cuetara <23455324+vdelacue@users.noreply.github.com>
2019-08-12 16:57:16 -0400

    calls to userprofile and my list complete and functional

--- public/assets/js/home.js
+++ public/assets/js/home.js
@@ -4,14 +4,22 @@ $(function () {
     // assigning white listed domains where it is allowed (ex: localhost)
     let provider = new firebase.auth.GoogleAuthProvider();
     let auth = firebase.auth();
+    //userArr is an array of 1 object that contains data from firebase authentication
     let userArr = [];
+    let userDBid = "";
 
-
-    //-----------------TARGETED HTML ELEMENTS USING JQuery-------------------//
+    //===================TARGETED HTML ELEMENTS USING JQuery===================//
     const $bucketProContainer = $('#user-profile-list');
-    const $userConsole = $('#"user-console"');
+    const $userConsole = $('#user-console');
+    const $advCatSelect = $();
+    const $traCatSelect = $();
+    const $eduCatSelect = $();
+    const $romCatSelect = $();
+    const $entCatSelect = $();
+    const $uniCatSelect = $();
+    const $allCatSelect = $();
 
-    //-----------------FUNCTIONS CALLED IN AJAX CALLS ----------------------//
+    //===================FUNCTIONS CALLED IN AJAX CALLS ===================//
     
     //function to diplay list items
     let myBucketList = function (activities) {
@@ -24,13 +32,14 @@ $(function () {
         }
     }
 
+    //--------function to display result list in console
     let myResultList = function(results){
         $userConsole.empty();
         for (let i = 0; i < results.length; i++) {
             let listItem = results[i];
             $bucketProContainer.append(`
-            <p class="" data-type="activity">${activity.activity}
-            <i class="fas fa-trash-alt ml-2" id="${activity.id}"></i></p>`)
+            <p class="" data-type="activity">${results.activity}
+            <i class="fa fa-plus ml-2" id="${activity.id}"></i></p>`)
         }
     }
 
@@ -38,12 +47,11 @@ $(function () {
 
 
 
-    //----------------AJAX API CALLS----------------//
+    //===================AJAX API CALLS===================//
 
     //AJAX Call to get my list JSON object from api
     let getJSONactivities = function () {
-        console.log(userArr[0])
-        let queryString = "/api/mylist/" + userArr[0];
+        let queryString = "/api/mylist/" + userArr[0].uid;
         $.ajax({
             url: queryString,
             method: "GET"
@@ -53,6 +61,27 @@ $(function () {
         });
     };
 
+    let getJSONUserProfile = function(uid){
+        let queryString = "/api/userprofile/" + uid;
+        $.ajax({
+            url: queryString,
+            method: "GET"
+        }).then(function (data) {
+            userDBid = data[0].id;
+        });
+    }
+
+    let getJSONResultList = function(catID){
+        let queryString = "/api/userprofile/" + catID;
+        $.ajax({
+            url: queryString,
+            method: "GET"
+        }).then(function (data) {
+            myResultList(data)
+        });
+    }
+
+    //function to get the current user from firebase and store in variable
     function getCurrentUser(auth) {
         return new Promise((resolve, reject) => {
             const unsubscribe = auth.onAuthStateChanged(user => {
@@ -62,18 +91,29 @@ $(function () {
         });
     }
     // store current user in userArr
-    getCurrentUser(auth).then(function (data) {
-        userArr.push(data.uid);
-        userArr.push(data.displayName);
-        userArr.push(data.photoURL);
-        console.log(userArr[0])
-        // Functions used to display content
+    getCurrentUser(auth).then(function (data) { 
+        userArr.push({
+            uid: data.uid, 
+            displayName: data.displayName,
+            photoURL: data.photoURL
+         });
+        // call Functions used to display content
+        getJSONactivities(userArr[0].uid);
+        getUserProfile(userArr[0].uid);
+        
+    });
 
 
 
-        getJSONactivities();
 
-    })
+    //===============Button Click Functions===============//
+
+    $
+
+   
+    
+
+
 
 
 })
