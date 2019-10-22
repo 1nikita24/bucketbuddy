45758b5119b28042c09c4ae9cf7668dc7d7fb2d1
Vanessa de La Cuetara <23455324+vdelacue@users.noreply.github.com>
2019-10-22 13:05:53 -0400

    added pointers icons and names to buckets

--- public/assets/js/home.js
+++ public/assets/js/home.js
@@ -54,7 +54,8 @@ $(function () {
                 $buddyIconRow.append(`
             <span class="budIcons"><a href="mailto:${data[i].email}?Subject=Hey%20Buddy%20Let's%20${data[i].activity}" class="circle" id="cir${i}">
             <img height="63" width="63"
-             src="${data[i].photoUrl}" alt="Buddy Avatar"></a></span>`)
+             src="${data[i].photoUrl}" alt="Buddy Avatar"></a></span>
+             <p>${data[i].name}</p>`)
             } else {
                 $buddyIconRow.append(`
             <span class="budIcons"><a href=#add-Buddy class="circle" id="cir${i}">
